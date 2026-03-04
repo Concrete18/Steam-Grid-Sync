551c6a4c280e77ae5f05b1c5c18c51a5bd50cba6
Concrete18 <michaelericson19@gmail.com>
2026-03-04 18:12:35 -0500

    finished base use of add_image func

--- utils/add_image.py
+++ utils/add_image.py
@@ -5,55 +5,86 @@ import tkinter as tk
 from tkinter import filedialog
 
 # third-party imports
-from pick import pick
+from PIL import Image
 
 
-def get_file_path() -> Path:
+def get_file_paths() -> list[Path]:
     """
     ph
     """
     root = tk.Tk()
     root.withdraw()
-    file_path = filedialog.askopenfilename(title="Select a file")
-    if file_path == ".":
-        return False
-    return Path(file_path)
+    files = filedialog.askopenfiles(title="Select a file")
+
+    paths = []
+    for file in files:
+        if file.name == ".":
+            continue
+        path = Path(file.name)
+        paths.append(path)
+    return paths
+
+
+def get_steam_image_type(image_path):
+    """
+    Gets the type of image for customizaing Steam game images.
+    """
+    with Image.open(image_path) as img:
+        width, height = img.size
+    res = f"{width} x {height}"
+    types = {
+        "1920 x 620": "hero",
+        "920 x 430": "active",
+        "600 x 900": "grid",
+    }
+    image_type = types.get(res, "logo")
+    return image_type
 
 
 def create_filename(steam_url: str, image_type: str, suffix: str) -> str:
     """
-    ph
+    Creates the new filename for a steam grid image.
     """
+    # TODO swap to using regex
     url_parts = steam_url.split("/")
-    game_name = url_parts[5]
-    if game_name:
-        game_name = game_name.replace("_", " ").title()
+    if len(url_parts) > 5:
+        game_name = url_parts[5].replace("_", " ").title()
     else:
-        game_name = input("What is the game's name?")
+        game_name = input("What is the game's name?\n")
     app_id = url_parts[4]
-    print(f"\nName: {game_name}\nApp ID: {app_id}")
     return f"{game_name}_{image_type}_{app_id}{suffix}"
 
 
-def add_image(custom_grid_image_dir: Path) -> None:
+def move_image(
+    image_path: Path,
+    final_filename: str,
+    custom_grid_image_dir: Path,
+) -> None:
+    """
+    Moves the image to the grid image directory so it can be
+    used to update Steam customization.
+    """
+    destination = custom_grid_image_dir / final_filename
+    if destination.exists():
+        # TODO add input to ask if it should be replaced
+        print(f"{final_filename} image already exists.")
+        return
+    shutil.move(image_path, destination)
+
+
+def add_images(custom_grid_image_dir: Path, image_paths=[]) -> None:
     """
     ph
     """
-    target = get_file_path()
-    if not target:
-        return
+    if not image_paths:
+        image_paths = get_file_paths()
+        if not image_paths:
+            return
 
     steam_url = input("\nWhat is the Steam Game URL?\n")
+    for image_path in image_paths:
+        image_type = get_steam_image_type(image_path)
+        final_filename = create_filename(steam_url, image_type, image_path.suffix)
+        move_image(image_path, final_filename, custom_grid_image_dir)
 
-    IMAGE_TYPES = ["Grid", "Hero", "Logo", "Active"]
-    index = pick(
-        IMAGE_TYPES,
-        "What type of Steam Grid image is this image?",
-        indicator="->",
-    )[1]
-
-    filename = create_filename(steam_url, IMAGE_TYPES[index].lower(), target.suffix)
-    destination = custom_grid_image_dir / filename
-
-    shutil.move(target, destination)
     print("\nNew Steam Grid Image Added")
