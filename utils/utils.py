af35a02957f1d6ab8d15e9e0af473dbb31528430
Concrete18 <michaelericson19@gmail.com>
2026-03-04 18:14:24 -0500

    improved utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,19 +1,19 @@
 import hashlib, os
 
 
-def hash_file(file_path: str) -> int:
+def hash_file(file_path: str) -> str:
     """
     Returns the images sha256 hash.
     """
     if not os.path.exists(file_path):
-        return None
+        return ""
     with open(file_path, "rb") as f:
         return hashlib.sha256(f.read()).hexdigest()
 
 
-def ask_for_yes_or_no(msg=""):
+def yes_or_no(msg=""):
     """
-    ph
+    Asks for user input and will return True if user inputs any for of yes.
     """
     response = input(msg)
-    return response.lower() in ["yes", "y"]
+    return response.lower() in ["yes", "y", "yeah"]
